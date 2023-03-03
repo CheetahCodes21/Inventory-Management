b07f7b168b20d50d2feecfe1754306b42a360c09
Cheetah2107 <mcpai217@gmail.com>
2023-03-03 14:11:04 +0530

    starting

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Homepage from './pages/Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage>
+        
+      </Homepage>
+    </div>
+  );
+}
+
+export default App;
